806b9f0506959b583e5e25441ee37893c0833477
NeuroBoy <91805629+a7medRo7oma@users.noreply.github.com>
2023-02-18 23:43:50 +0200

    displayDetails

--- Contact.js
+++ Contact.js
@@ -1,8 +1,10 @@
+
 class Contact {
   constructor(name, phone, email, gender) {
     this.name = name;
     this.phone = phone;
     this.email = email;
     this.gender = gender;
+    this.id = phone
   }
 }
--- PhoneBook.js
+++ PhoneBook.js
@@ -16,20 +16,20 @@ $(document).ready(function () {
     contacts.push(contact);
     localStorage.setItem('contacts-list', JSON.stringify(contacts));
     appendToListView(contact);
+    // clearInputs();
   });
-
   initListview();
 });
 
 function initListview() {
   console.log(contacts);
   for (let i = 0; i < contacts.length; i++) {
-    $('#contacts-listview').append(`
-  <li><a href="#contact-details">
-  <img src="images/${contacts[i].gender}.png">
-  <h2>${contacts[i].name}</h2>
-  <p>${contacts[i].phone}</p>
-</a>
+    $('#contacts-listview').append(`<li><a onclick="displayDetails(${contacts[i].id},)" href="#contact-details">
+    <img src= "images/${contacts[i].gender}.png">
+    <h2>${contacts[i].name}</h2>
+    <p>${contacts[i].phone}</p>
+    </a>
+<a href="tel:${contacts[i].phone}"  class = "ui-btn ui-btn-inline ui-icon-phone ui-btn-icon-notext"></a>
 </li>`);
   }
 
@@ -38,12 +38,38 @@ function initListview() {
 
 function appendToListView(contact) {
   $('#contacts-listview').append(`
-  <li><a href="#contact-details">
+  <li><a href="#contact-details" onclick="displayDetails(${contacts[i].id},)">
   <img src="images/${contact.gender}.png">
   <h2>${contact.name}</h2>
   <p>${contact.phone}</p>
 </a>
+<a href="tel:${contacts[i].phone}"  class = "ui-btn ui-btn-inline ui-icon-phone ui-btn-icon-notext"></a>
 </li>`);
 
   $('#contacts-listview').listview('refresh');
 }
+
+function displayDetails(id) {
+  console.log("clicked");
+  // console.log();
+
+  let contact = contacts.filter(c => c.id == id)[0];
+
+  console.log(contact);
+  $('#details-name').text(contact.name);
+  // $('#details-phone').text(contact.phone);
+  $('#details-image').attr("src", `images/${contact.gender}.png`);
+  $('#details-callPhone').attr("href", `tel:${contact.phone}`);
+  console.log(contact.id);
+
+}
+
+
+
+
+// function clearInputs() {
+//   $('#textinput-name').val() = "";
+//   $('#textinput-phone').val() = "";
+//   $('#textinput-email').val() = "";
+//   $('#select-gender').val() = "";
+// }
