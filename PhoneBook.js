acd1fff462539de0257b16f097730daa5720b262
zaki <abdulrahman.zaki.342@gmail.com>
2023-02-19 15:11:07 +0200

    ed

--- PhoneBook.js
+++ PhoneBook.js
@@ -1,13 +1,16 @@
 let contacts;
-
+let currentId = -1 ;
 $(document).ready(function () {
   let contactsData = localStorage.getItem('contacts-list');
   contacts = contactsData ? JSON.parse(contactsData) : [];
   //Add new Contact
   $('#save-button').bind('click', function (event, ui) {
     event.preventDefault();
-    document.querySelector('#textinput-email').checkValidity();
-    document.querySelector('#textinput-email').reportValidity();
+    let nameValid = document.querySelector('#textinput-name').checkValidity();
+    document.querySelector('#textinput-name').reportValidity();
+    let phoneValid = document.querySelector('#textinput-phone').checkValidity();
+    document.querySelector('#textinput-phone').reportValidity();
+    if(!nameValid || !phoneValid) return;
     let name = $('#textinput-name').val();
     let phone = $('#textinput-phone').val();
     let email = $('#textinput-email').val();
@@ -17,6 +20,7 @@ $(document).ready(function () {
     localStorage.setItem('contacts-list', JSON.stringify(contacts));
     appendToListView(contact);
     clearInputs();
+    // window.location.href ="#contacts-list";
   });
   initListview();
 });
@@ -26,6 +30,7 @@ function initListview() {
   for (const contact of contacts) {
     appendToListView(contact);
   }
+  $('#contacts-listview').listview('refresh');
 }
 
 function appendToListView(contact) {
@@ -38,10 +43,13 @@ function appendToListView(contact) {
 <a href="tel:${contact.phone}"  class = "ui-btn ui-btn-inline ui-icon-phone ui-btn-icon-notext"></a>
 </li>`);
 
-  $('#contacts-listview').listview('refresh');
+$('#contacts-listview').listview('refresh');
+
+ 
 }
 
 function displayDetails(id,ui) {
+  currentId = id ;
   console.log(ui);
   console.log('clicked');
   let contact = contacts.filter((c) => c.id == id)[0];
@@ -73,4 +81,40 @@ function deleteContact(id){
   localStorage.setItem('contacts-list', JSON.stringify(contacts));
   $(`#${id}`).remove();
   window.location.href ="#contacts-list";
+  $('#contacts-listview').listview('refresh');
 }
+
+function EditClick(){
+  console.log(currentId);
+  let index = contacts.findIndex(c => c.id == currentId);
+  let contact = contacts[index];
+  $('#textinput-name-edit').val(contact.name);
+  $('#textinput-phone-edit').val(contact.phone);
+  $('#textinput-email-edit').val(contact.email);
+  $('#select-gender-edit').val(contact.gender);
+
+}
+
+function updateContact(event){
+  event.preventDefault();
+  console.log(currentId);
+  let index = contacts.findIndex(c => c.id == currentId);
+  let contact = contacts[index];
+  contact.name = $('#textinput-name-edit').val();
+  contact.phone = $('#textinput-phone-edit').val();
+  contact.email = $('#textinput-email-edit').val();
+  contact.gender = $('#select-gender-edit').val();
+  localStorage.setItem('contacts-list', JSON.stringify(contacts));
+  window.location.href = "#contacts-list"
+    // console.log( $(contact.id ).children());
+    $(`#${contact.id} h2`).text(contact.name);
+    $(`#${contact.id} p`).text(contact.phone);
+    $(`#${contact.id} img`).attr('src', `images/${contact.gender}.png`);
+}
+
+// function validateAddContact(){
+//       document.querySelector('#textinput-name').checkValidity();
+//     document.querySelector('#textinput-name').reportValidity();
+//     document.querySelector('#textinput-phone').checkValidity();
+//     document.querySelector('#textinput-phone').reportValidity();
+// }
